310b591f47821a2671cd6fc60fc1bc4effef70c7
Kevin Yokley <kyokley2@gmail.com>
2017-01-11 17:40:18 -0600

    Updating tests

--- tests/test_confuse.py
+++ tests/test_confuse.py
@@ -26,6 +26,42 @@ class TestConfuse(unittest.TestCase):
         actual = confuse(sample_str)
         self.assertEqual(expected, actual)
 
+    def test_integer_substitution(self):
+        sample_str = '%i apples and %i oranges'
+        expected = u'%i \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %i \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
+    def test_octal_substitution(self):
+        sample_str = '%o apples and %o oranges'
+        expected = u'%o \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %o \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
+    def test_lowercase_hex_substitution(self):
+        sample_str = '%x apples and %x oranges'
+        expected = u'%x \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %x \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
+    def test_uppercase_hex_substitution(self):
+        sample_str = '%X apples and %X oranges'
+        expected = u'%X \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %X \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
+    def test_number_substitution(self):
+        sample_str = '%n apples and %n oranges'
+        expected = u'%n \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %n \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
+    def test_binary_substitution(self):
+        sample_str = '%b apples and %b oranges'
+        expected = u'%b \xe3\u1d7d\u1d7d\u0142\xeb\u015f \xe3\xf1\u1d81 %b \xf0\u0155\xe3\xf1\u011f\xeb\u015f'
+        actual = confuse(sample_str)
+        self.assertEqual(expected, actual)
+
     def test_keyword_substitution(self):
         sample_str = '%(first)s, %(second)s, or %(third)s'
         expected = u'%(first)s\u275f %(second)s\u275f \xf0\u0155 %(third)s'
